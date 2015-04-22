2ce95e36296202b72616e2566369f9c672f3b797
predat <sylvain@predat.fr>
2015-04-22 12:13:21 +0200

    More easy import

--- pyIsis/__init__.py
+++ pyIsis/__init__.py
@@ -3,12 +3,13 @@
 #from .conn import *
 #from .client import *
 
-__all__ = ['client','connexion','utils','filesystem']
-
+__all__ = ['client','connection','utils','filesystem']
 __author__ = "Sylvain Maziere"
-
 __version__ = '0.1.0'
 __license__ = "GPL"
 __maintainer__ = "Sylvain Maziere"
 __email__ = "sylvain@predat.fr"
 __status__ = "Development"
+
+from conection import SOAPConnection, WEBConnection
+
