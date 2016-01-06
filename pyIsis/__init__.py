a4965725dfd7c665bc252e00a8ecdabdfae97735
predat <sylvain@predat.fr>
2016-01-06 11:39:42 +0100

    bump version

--- pyIsis/__init__.py
+++ pyIsis/__init__.py
@@ -5,7 +5,7 @@
 
 __all__ = ['client','connection','utils','filesystem']
 __author__ = "Sylvain Maziere"
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 __license__ = "GPL"
 __maintainer__ = "Sylvain Maziere"
 __email__ = "sylvain@predat.fr"
